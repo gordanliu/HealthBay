418ddcf8479730ae82d7212552de766f47e5ddee
DonovanY <35472374+DonovanY@users.noreply.github.com>
2025-11-08 01:03:27 -0500

    Backend setup complete: express server running, routes and db config online

---
+++ client/src/api/chatApi.js
@@ -0,0 +1,7 @@
+// client/src/api/chatApi.js
+import api from "./apiClient";
+
+export async function sendChatMessage(question) {
+  const res = await api.post("/chat", { question });
+  return res.data;
+}
